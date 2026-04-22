64b86c2b9f0be1633ce123b8cc828ccb5138de9d
uros1992 <theman.uros@gmail.com>
2026-04-22 11:46:44 +0200

    feat: Add schema.org WebSite markup
    Includes the WebSite schema for SEO and updates fetchTest.ts to use the root URL.

--- fetchTest.ts
+++ fetchTest.ts
@@ -1,6 +1,6 @@
 import https from 'https';
 
-https.get('https://euro-uvac.onrender.com/sitemap.xml', (res) => {
+https.get('https://euro-uvac.onrender.com/', (res) => {
   console.log('STATUS:', res.statusCode);
   console.log('HEADERS:', res.headers);
   res.on('data', () => {});
